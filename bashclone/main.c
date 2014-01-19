8ed0abee9661da4cc0931037bcd9ca065a57146f
abcxq <abcxq@21cn.com>
2014-01-19 11:10:00 +0800

    just add bashclone

---
+++ bashclone/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "shell.h"
+
+
+
+
+int main(void)
+{
+    char buf[64];
+    int argc = 0;
+    char * argv[10];
+
+	while(1)
+	{
+		printf("# ");
+		fgets(buf, 64, stdin);
+		buf[strlen(buf)-1] = '\0';
+
+		argc = shell_parse(buf, argv);
+		
+		do_cmd(argc, argv);
+
+	}
+
+
+
+    return 0;
+}
+
