84ed27b03a5625fd8f2747101e38604e62b706fb
abcxq <abcxq@21cn.com>
2014-03-18 15:24:07 +0800

    just add binserch

--- algo/binserch/binser.c
+++ algo/binserch/binser.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <search.h>
 
 #define NELEMS(arr) (sizeof(arr) / sizeof(arr[0]))
-int numarray[] = {123, 145, 512, 627, 800, 933};
-const int key = 512;
+const int a[6] = {123,145,512,627,800,933};
+const int key = 123;
 
 
 void *binse(const void *key, const void *base, size_t nelem, size_t width, int(*fcmp)(const void *, const void *))
 {
-	void *low,*high,*mid;
-	low = base;
-	high = base + (nelem - 1) * width;
+	char  *low,*high,*mid;
+	low = (char *)base;
+	high = (char *)base + (nelem - 1) * width;
+
 
 	while(low <= high)
 	{
-		mid = (high - low)/2;
-		
-		if (fcmp(key,mid) < 0)
+		nelem = (high - low) / width + 1;
+		printf("nelem is %d",nelem);
+		mid = low +  (nelem - 1 ) / 2 * width ; //important
+//		printf("low is %d mid is %d\n",*low,*(int *)mid);
+		if (fcmp((char *)key,mid) < 0)
 		{
-			high = mid + width;
+			high = mid - width; //important
 		}
 		else
-			if (fcmp(key,mid) > 0)
-				low = mid + width;	
+			if (fcmp((char *)key,mid) > 0)
+			{
+				low = mid + width; //important
+			//	printf("low is %d, high is %d\n",*(int *)low,*(int *)high);
+			}	
 			else
 				return mid;
 	}
@@ -30,19 +38,18 @@ void *binse(const void *key, const void *base, size_t nelem, size_t width, int(*
 	return NULL;
 }
 	
-int numeric(const int *p1, const int *p2)
+int numeric(const void *p1, const void *p2)
 {
-	printf("p1 is %x,p2 is %x",p1,p2);
-			printf("yyy\n");
 
-	return(*p1 - *p2);
+	return *(int *)p1 - *(int *)p2;
 }
 
 int main()
 {
 	int *itemptr = NULL;
+	printf("key is %d \n",(char *)key);	
 
-	itemptr = (int *)binse(&key,numarray,NELEMS(numarray),sizeof(int),numeric);
+	itemptr = (int *)binse(&key,a,6,sizeof(int),numeric);
 	
 
 	if (itemptr == NULL)
